e6645db7970c9ab86ce0744d6f43d4f37c489539
Kidd-nash <arkady.pantaleon@gmail.com>
2024-06-20 21:38:03 +0800

    NumbersAndStrings_

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log(23);
+// these two slashes are used to make single comments
+/* these slashes and asterisks are used for comments
+   that are quite long compared to short comments
+*/
+console.log('String that has capital, spaces, punctation and multiple words.');
+// string usually have quotes inside the parenthesis
+console.log(2011);
+// however, numbers does not need quotes inside the parenthesis
