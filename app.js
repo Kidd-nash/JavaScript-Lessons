789dfeddd1a9ae997130ab78bdd7e072c0dba5dd
Kidd-nash <arkady.pantaleon@outlook.com>
2024-06-25 09:05:00 +0800

    variables

--- app.js
+++ app.js
@@ -26,3 +26,9 @@ console.log('Codecademy'.toUpperCase());
 console.log(Math.floor(Math.random() * 100));
 console.log(Math.ceil(43.8));
 console.log(Number.isInteger(2017));
+//next is creating variables
+var bestGundam = 'GundamWing';
+console.log(bestGundam):
+//pb
+var numOfGundamWings = 2;
+console.log(numOfGundamWings);
