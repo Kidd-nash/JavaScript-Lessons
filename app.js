4e9e363e77b36ab2fff22b73df723d150f5b01be
Kidd-nash <arkady.pantaleon@outlook.com>
2024-06-28 13:17:19 +0800

    Switch_Keywords

--- app.js
+++ app.js
@@ -205,3 +205,19 @@ if (season === 'spring') {
 } else {
   console.log('Invalid season.');
 }
+//The switch keywords
+let athleteFinalPosition = 'first place';
+switch (athleteFinalPosition) {
+  case 'first place':
+    console.log('You get the gold medal!');
+    break;
+  case 'second place':
+    console.log('You get the silver medal!');
+    break;
+  case 'third place':
+    console.log('You get the bronze medal!');
+    break;
+  default:
+    console.log('No medal awarded.');
+    break;
+}
