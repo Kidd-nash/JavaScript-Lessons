4de44d6340cb9917d0af312ba71d38f067f23c17
Kidd-nash <arkady.pantaleon@outlook.com>
2024-06-28 11:12:50 +0800

    Ternary_Operators

--- app.js
+++ app.js
@@ -181,3 +181,13 @@ let tool = 'marker';
 let writingUtensil= tool || 'pen';
 
 console.log(`The ${writingUtensil} is mightier than the sword.`);
+//Ternary Operators
+isLocked ? console.log('You will need a key to open the door.') :
+console.log('You will not need a key to open the door.');
+
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+
+favoritePhrase ==='Love That!' ? console.log('I love that!') :
+console.log("I don't love that!");
