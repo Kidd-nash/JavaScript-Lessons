61e44842c69619979ac920b6a0e0d1b8ab1c0133
Kidd-nash <arkady.pantaleon@outlook.com>
2024-06-28 10:02:15 +0800

    Logical_Operators

--- app.js
+++ app.js
@@ -134,3 +134,16 @@ if(hungerLevel > 7) {
 } else {
   console.log('We can eat later!');
 }
+//Logical Operators
+/*
+the and operator (&&)
+the or operator (||)
+the not operator, otherwise known as the bang operator (!)
+*/
+let mood = 'sleepy';
+let tirednessLevel = 6;
+if (mood === 'sleepy' && tirednessLevel > 8) {
+  console.log('time to sleep');
+} else {
+  console.log('not bed time yet');
+}
