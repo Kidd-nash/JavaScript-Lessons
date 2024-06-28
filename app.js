3703d398acae6f5f0f4daa094e4cb3aa4a216332
Kidd-nash <arkady.pantaleon@outlook.com>
2024-06-28 10:20:13 +0800

    Truthy_&_Falsy

--- app.js
+++ app.js
@@ -147,3 +147,30 @@ if (mood === 'sleepy' && tirednessLevel > 8) {
 } else {
   console.log('not bed time yet');
 }
+//Truthy and Falsy
+/*
+So which values are falsy— or evaluate to false when checked as a condition?
+The list of falsy values includes:
+
+0
+Empty strings like "" or ''
+null which represent when there is no value at all
+undefined which represent when a declared variable lacks a value
+NaN, or Not a Number
+*/
+let wordCount = 2;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = '';
+
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
