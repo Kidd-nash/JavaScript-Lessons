0c04f4ee8a56eba6d683e799467ad7927785e342
Kidd-nash <arkady.pantaleon@outlook.com>
2024-07-04 17:18:25 +0800

    Function - Helper Functions

--- app.js
+++ app.js
@@ -329,3 +329,15 @@ function monitorCount(rows, columns) {
 }
  const numOfMonitors = monitorCount(5, 4);
  console.log(numOfMonitors);
+
+ //Helper Functions
+ function monitorCount(rows, columns) {
+  return rows * columns;
+};
+function costOfMonitors(rows, columns) {
+  return monitorCount(rows, columns) * 200;
+};
+
+const totalCost = costOfMonitors(5, 4);
+
+console.log(totalCost);
