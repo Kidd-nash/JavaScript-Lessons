c0e14c71ba27af5605c9f78e63f748095f3c6fb6
Kidd-nash <arkady.pantaleon@outlook.com>
2024-07-04 16:46:59 +0800

    Function - Return

--- app.js
+++ app.js
@@ -322,3 +322,10 @@ function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
   console.log(`Remember to buy ${item2}`);
   console.log(`Remember to buy ${item3}`);
 }
+//Returns
+function monitorCount(rows, columns) {
+
+  return rows * columns;
+}
+ const numOfMonitors = monitorCount(5, 4);
+ console.log(numOfMonitors);
