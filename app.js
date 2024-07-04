0fef03955f09c35e7a5ffc0b634012e0cc9a1f31
Kidd-nash <arkady.pantaleon@outlook.com>
2024-07-04 16:16:41 +0800

    Function - Default Parameters

--- app.js
+++ app.js
@@ -308,11 +308,17 @@ function greetInSpanish() {
   console.log('Buenas tardes');
 }
 
+//calling a function
 getReminder();
 greetInSpanish();
-
+//Parameters and Arguments
 function sayThanks(name) {
   console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
 }
-
 sayThanks('Cole');
+//Default Parameters
+function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+}
