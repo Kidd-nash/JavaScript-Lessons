8cc9343fd447ec29a3b89a39b16ce9a513dba412
Kidd-nash <arkady.pantaleon@outlook.com>
2024-07-05 14:11:47 +0800

    Scope - Global Scope

--- app.js
+++ app.js
@@ -368,3 +368,14 @@ const logCitySkyline = () => {
  return 'The stars over the ' + skyscraper + ' in ' + city;
 }
 console.log(logCitySkyline());
+//Global Scope
+
+var satellite = 'The Moon';
+var galaxy = 'The Milky Way';
+var stars = 'North Star';
+
+const callMyNightSky = () => {
+  return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+}
+
+console.log(callMyNightSky());
