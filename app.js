98cfdb3d2a102f445caa3681c5fbe1bdce1dc08d
Kidd-nash <arkady.pantaleon@outlook.com>
2024-07-09 11:11:01 +0800

    Arrays - while loops

--- app.js
+++ app.js
@@ -527,3 +527,12 @@ for (let i = 0; i < bobsFollowers.length; i++) {
     }
   }
 };
+//The while Loop
+const cards = ['diamond', 'spade', 'heart', 'club'];
+
+let currentCard = [];
+
+while (currentCard !== 'spade') {
+  currentCard = cards[Math.floor(Math.random() * 4)];
+  console.log(currentCard);
+};
