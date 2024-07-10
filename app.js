201d364bbeaaff05b99734f5d10ab694c3b6c77d
Kidd-nash <arkady.pantaleon@outlook.com>
2024-07-10 17:06:43 +0800

    Iterators, .map

--- app.js
+++ app.js
@@ -588,3 +588,17 @@ const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
 fruits.forEach (function(fruits) {
   console.log('I want to eat a ' + fruits);
 });
+//The .map method
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+const secretMessage = animals.map(
+  firstCharacter => firstCharacter.substring(0, 1)
+);
+
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+const smallNumbers = bigNumbers.map(num => num/100);
+
+console.log(smallNumbers)
