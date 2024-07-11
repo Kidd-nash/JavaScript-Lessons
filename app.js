29ddb2449493956866705613f01fbfb599077153
Kidd-nash <arkady.pantaleon@outlook.com>
2024-07-11 14:59:32 +0800

    Objects, looping

--- app.js
+++ app.js
@@ -794,3 +794,34 @@ greenEnergy(spaceship);
 remotelyDisable(spaceship);
 
 console.log(spaceship)
+//Looping through objects
+let spaceship = {
+    crew: {
+    captain: {
+        name: 'Lily',
+        degree: 'Computer Engineering',
+        cheerTeam() { console.log('You got this!') }
+        },
+    'chief officer': {
+        name: 'Dan',
+        degree: 'Aerospace Engineering',
+        agree() { console.log('I agree, captain!') }
+        },
+    medic: {
+        name: 'Clementine',
+        degree: 'Physics',
+        announce() { console.log(`Jets on!`) } },
+    translator: {
+        name: 'Shauna',
+        degree: 'Conservation Science',
+        powerFuel() { console.log('The tank is full!') }
+        }
+    }
+};
+
+for (let role in spaceship.crew) {
+    console.log(`${role}: ${spaceship.crew[role].name}`);
+}
+for (let crewMember in spaceship.crew) {
+  console.log(`${spaceship.crew[crewMember].name}: ${spaceship.crew[crewMember].degree}`)
+};
