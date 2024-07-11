7292917c445e9e535e345f16214644d3ffdf68dd
Kidd-nash <arkady.pantaleon@outlook.com>
2024-07-11 14:50:47 +0800

    Objects, pass by reference

--- app.js
+++ app.js
@@ -775,3 +775,22 @@ spaceship.passengers = [
   }
 ];
 var firstPassenger = spaceship.passengers[0];
+//Pass by reference
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth'
+};
+
+// Write your code below
+function greenEnergy(obj) {
+    obj['Fuel Type'] = 'avocado oil';
+}
+function remotelyDisable(obj) {
+    obj.disabled = true;
+}
+
+greenEnergy(spaceship);
+
+remotelyDisable(spaceship);
+
+console.log(spaceship)
