5947a643559b775d4f1962ae092f7867f3e56156
Kidd-nash <arkady.pantaleon@outlook.com>
2024-07-11 13:18:13 +0800

    Objects, creating object literals

--- app.js
+++ app.js
@@ -681,3 +681,8 @@ const smallerNums = nums.map(num => num - 5);
 
 // Choose a method that will return a boolean value
 nums.some(num => num < 0);
+//Creating Object Literals
+let fasterShip = { //a fast ship
+  'Fuel Type': 'Turbo Fuel',
+  color: 'silver'
+};
