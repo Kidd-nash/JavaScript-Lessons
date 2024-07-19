2afa984ee35f92cd4757ea9cccd63c0e985a2086
Kidd-nash <arkady.pantaleon@outlook.com>
2024-07-19 10:12:24 +0800

    Class - Inheritance

--- app.js
+++ app.js
@@ -895,3 +895,19 @@ const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics');
 console.log(surgeonRomero.name);
 surgeonRomero.takeVacationDays(3);
 console.log(surgeonRomero.remainingVacationDays);
+//Inheritance II
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+  get name() {
+    return this._name;
+  }
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
