2fa2b99c65f4723adb817baa5b327a4b57735442
Kidd-nash <arkady.pantaleon@outlook.com>
2024-07-29 13:22:37 +0800

    React - nesting elements

--- react-lesson.js
+++ react-lesson.js
@@ -1,11 +1,11 @@
-//Hybrid from of HTML and JS(a.k.a JSX)
-const h1 = <h1>Hello world</h1>;
+  //Hybrid from of HTML and JS(a.k.a JSX)
+  const h1 = <h1>Hello world</h1>;
 //JSX Elements
-<p>Hi world</p>
-//any html elements would do, just use proper elements or classes if necessary 
+  <p>Hi world</p>
+  //any html elements would do, just use proper elements or classes if necessary 
 //JSX Elements and their surrounding
-let myArticle = <article></article>;
-const myTeam = {
+  let myArticle = <article></article>;
+  const myTeam = {
     center: <li>Jokix</li>,
     powerForward: <li>Shaq</li>,
     smallForward: <li>James</li>,
@@ -13,10 +13,14 @@ const myTeam = {
     pointGuard: <li>CP3</li>
   };
   //elements can be inside a variable, object/array, even classes
-  //Attributes in JSX
-  const p1 = <p
-            id="large"
-            >food</p>;
-const p2 = <p
-            id="small"
-            >bar</p>;
+//Attributes in JSX
+  const p1 = <p id="large">food</p>;
+  const p2 = <p id="small">bar</p>;
+//Nested elements in JSX
+  let myDiv = (
+    <div>
+      <h1>
+        Hello world
+      </h1>
+    </div>
+  );
