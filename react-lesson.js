374ee7661b4ad49f298fd851dabcb1dc43a15aad
Kidd-nash <arkady.pantaleon@outlook.com>
2024-07-29 10:31:04 +0800

    React - elements surrounding

--- react-lesson.js
+++ react-lesson.js
@@ -4,3 +4,12 @@ const h1 = <h1>Hello world</h1>;
 <p>Hi world</p>
 //any html elements would do, just use proper elements or classes if necessary 
 //JSX Elements and their surrounding
+let myArticle = <article></article>;
+const myTeam = {
+    center: <li>Jokix</li>,
+    powerForward: <li>Shaq</li>,
+    smallForward: <li>James</li>,
+    shootingGuard: <li>Curry</li>,
+    pointGuard: <li>CP3</li>
+  };
+  //elements can be inside a variable, object/array, even classes
