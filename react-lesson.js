4f7340588b4b26a8cb4bcfeb8108e49caffc45fa
Kidd-nash <arkady.pantaleon@outlook.com>
2024-07-30 12:24:32 +0800

    advanced jsx - conditionals

--- react-lesson.js
+++ react-lesson.js
@@ -140,3 +140,25 @@ root.render(myDivTwo);
 
   root.render(kitty);
 
+//JSX Conditionals
+// you can not inject an if statement into a JSX expression.
+  function coinToss() {
+    // This function will randomly return either 'heads' or 'tails'.
+    return Math.random() < 0.5 ? 'heads' : 'tails';
+  }
+
+  const pics = {
+    kitty: 'https://content.codecademy.com/courses/React/react_photo-kitty.jpg',
+    doggy: 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg'
+  };
+  let img;
+
+  // if/else statement begins here:
+  if (coinToss() === 'heads') {
+      img = <img src={pics.kitty} />;
+  } else {
+      img = <img src={pics.doggy} />;
+  };
+  root.render(img);
+  // The if statement is on the outside, and no JavaScript injection is necessary.
+
