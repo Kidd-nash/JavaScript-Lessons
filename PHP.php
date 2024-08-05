e3f0e19c029512d8e74e1ea9671521bd7343a50d
Kidd-nash <arkady.pantaleon@outlook.com>
2024-08-05 15:19:13 +0800

    PHP Numbers - in variables, 4 basic operations and exponentiations

--- PHP.php
+++ PHP.php
@@ -122,4 +122,43 @@ $language = "PHP Language";
 
 echo "My nickname is " . $name;
 echo "\nA string " . $language;  
-?>                        
+?>         
+
+<!-- Parsing variables -->
+<?php                   
+// Fill in the blanks in the code below:
+  $noun = "noun";
+  $adjective = "adjective";
+  $verb = "verb";
+
+  echo "The world's most beloved $noun was very $adjective and loved to $verb every single day.";
+
+
+
+//there is also this trick to add a few letters to your variables
+ echo "\nI have always been obsessed with ${noun}s. I'm ${adjective}ish. I'm always ${verb}ing.";
+ 
+ //Concatenating assignment operator
+ echo "I'm going on a picnic!";
+
+ $sentence = nl2br("\nI'm going on a picnic, and I'm taking apples");
+
+ echo $sentence;
+
+ $sentence .= ", banana";
+
+ echo $sentence;
+
+ $sentence .= ", cherry, and dragon fruit";
+
+ echo $sentence;
+   
+ 
+ ?>
+
+ <!-- PHP Numbers
+  Integer and Floating type, integers being the positive and negative whole numbers
+  floating being the decimal and fraction/ratio numbers.  -->
+  
+
+ 
