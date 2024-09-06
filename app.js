59d7d3de8f81d5b8f289be409c3bba81ed4415d5
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-06 11:19:44 +0800

    Advanced Objects - Built-in Object Methods

--- app.js
+++ app.js
@@ -1044,7 +1044,7 @@ console.log(nurseOlynyk.certifications);
 
 
   // To check that the property value shorthand technique worked:
-  const newRobot = robotFactory('P-501', false)
+  const aNewRobot = robotFactory('P-501', false)
   console.log(newRobot.model)
   console.log(newRobot.mobile)
 
@@ -1067,3 +1067,26 @@ console.log(nurseOlynyk.certifications);
 
   functionality.beep();
 
+//Built-in Object Methods
+
+  const robot = {
+    model: 'SAL-1000',
+    mobile: true,
+    sentient: false,
+    armor: 'Steel-plated',
+    energyLevel: 75
+  };
+
+  // What is missing in the following method call?
+  const robotKeys = Object.keys(robot);
+
+  console.log(robotKeys);
+
+  // Declare robotEntries below this line:
+  const robotEntries = Object.entries(robot)
+  console.log(robotEntries);
+
+  // Declare newRobot below this line:
+  const newRobot = Object.assign({laserBlaster: true, voiceRecognition: true}, robot);
+
+  console.log(newRobot);
