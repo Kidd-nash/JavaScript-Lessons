b1edd71a53615a6b7189fac97696d944090fcc70
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-06 11:00:19 +0800

    Advanced Objects - Setters

--- app.js
+++ app.js
@@ -974,6 +974,8 @@ console.log(nurseOlynyk.certifications);
   robot._energyLevel = 'sustainable ';
   robot.recharge();
 
+  // Getters
+
   const robotFour = {
     _model: '1E78V2',
     _energyLevel: 100,
@@ -987,5 +989,30 @@ console.log(nurseOlynyk.certifications);
   };
   
   console.log(robot.energyLevel);
+
+//Setters
+
+  const robotFive = {
+    _model: '1E78V2',
+    _energyLevel: 100,
+    _numOfSensors: 15,
+    get numOfSensors(){
+      if(typeof this._numOfSensors === 'number'){
+        return this._numOfSensors;
+      } else {
+        return 'Sensors are currently down.'
+      }
+    },
+    set numOfSensors(num) {
+      if (typeof num === 'number' && num >= 0){
+        this._numOfSensors = num;
+      } else {
+        console.log('Pass in a number that is greater than or equal to 0')
+      }   
+    } 
+  };
+
+  robot.numOfSensors = 100;
+  console.log(robot.numOfSensors);
 
 
