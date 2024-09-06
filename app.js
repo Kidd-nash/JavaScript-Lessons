0b8f81e2e7126eded01193ca76101e97c7075d61
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-06 11:06:15 +0800

    Advanced Objects - Factory Functions

--- app.js
+++ app.js
@@ -1015,4 +1015,19 @@ console.log(nurseOlynyk.certifications);
   robot.numOfSensors = 100;
   console.log(robot.numOfSensors);
 
+//Factory Functions
+
+  const robotFactory = (model, mobile) => {
+    return {
+      model : model,
+      mobile: mobile,
+      beep () { 
+        console.log('Beep Boop'); 
+      }
+    };
+  };
+
+  const tinCan = robotFactory('P-500', true);
+  tinCan.beep();
+
 
