b8ac3dfc7726589aed153e0d914d5aaedf725d7e
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-06 11:08:26 +0800

    Advanced Objects - Property Value Shorthand

--- app.js
+++ app.js
@@ -1030,4 +1030,21 @@ console.log(nurseOlynyk.certifications);
   const tinCan = robotFactory('P-500', true);
   tinCan.beep();
 
+//Property Value Shorthand
+
+  const robotFactoryTwo = (model, mobile) => {
+    return {
+      model,
+      mobile,
+      beep() {
+        console.log('Beep Boop');
+      }
+    }
+  }
+
+
+  // To check that the property value shorthand technique worked:
+  const newRobot = robotFactory('P-501', false)
+  console.log(newRobot.model)
+  console.log(newRobot.mobile)
 
