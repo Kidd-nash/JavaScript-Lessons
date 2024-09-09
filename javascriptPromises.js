365b968a44af747f5d979e674958eb03904eaebc
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-09 10:17:59 +0800

    JS Promise - using Promise.all()

--- javascriptPromises.js
+++ javascriptPromises.js
@@ -102,8 +102,6 @@
     giftcardBalance: 79.82
     };
 
-    // Refactor the code below:
-
     checkInventory(orderFour)
     .then((resolvedValueArray) => {
     return processPayment(resolvedValueArray);
@@ -114,3 +112,24 @@
     .then((successMessage) => {
     console.log(successMessage);
     });
+
+//Using Promise.all()
+
+    const {checkAvailability} = require('./library.js');
+
+    const onFulfill = (itemsArray) => {
+    console.log(`Items checked: ${itemsArray}`);
+    console.log(`Every item was available from the distributor. Placing order now.`);
+    };
+
+    const onReject = (rejectionReason) => {
+        console.log(rejectionReason);
+    };
+
+    const checkSunglasses = checkAvailability('sunglasses', 'Favorite Supply Co.');
+    const checkPants = checkAvailability('pants', 'Favorite Supply Co.'); 
+    const checkBags = checkAvailability('bags', 'Favorite Supply Co.');
+
+    Promise.all([checkSunglasses, checkPants, checkBags])
+    .then(onFulfill)
+    .catch(onReject);
