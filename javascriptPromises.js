efff0ed7b98f2350abf54f575bc95030eef5ccc2
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-09 09:41:59 +0800

    JS Promise - Node setTimeout() function

--- javascriptPromises.js
+++ javascriptPromises.js
@@ -22,3 +22,15 @@ const inventory = {
   const orderPromise = orderSunglasses();
   
   console.log(orderPromise);  
+
+//Node setTimeout() Function
+
+console.log("This is the first line of code in app.js.");
+
+const usingSTO = () => {
+  console.log("This line of code will log to the console last.");
+};
+
+setTimeout(usingSTO, 100);
+
+console.log("This is the last line of code in app.js.");
