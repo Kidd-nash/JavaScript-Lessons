5fad265746480fc26b309b5d55975236d3f7402d
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-09 09:44:38 +0800

    JS Promise - success and failure callback functions

--- javascriptPromises.js
+++ javascriptPromises.js
@@ -1,36 +1,53 @@
 
 //Constructing a Promise Object
 
-const inventory = {
-    sunglasses: 1900,
-    pants: 1088,
-    bags: 1344
-  };
-  
-  const myExecutor = (resolve, reject) => {
-      if (inventory.sunglasses > 0) {
-          resolve('Sunglasses order processed.');
-      } else {
-          reject('That item is sold out.');
-      }
-  };
-  
-  const orderSunglasses = () => {
-      return new Promise(myExecutor);
-  };
-  
-  const orderPromise = orderSunglasses();
-  
-  console.log(orderPromise);  
+    const inventory = {
+        sunglasses: 1900,
+        pants: 1088,
+        bags: 1344
+    };
+    
+    const myExecutor = (resolve, reject) => {
+        if (inventory.sunglasses > 0) {
+            resolve('Sunglasses order processed.');
+        } else {
+            reject('That item is sold out.');
+        }
+    };
+    
+    const orderSunglasses = () => {
+        return new Promise(myExecutor);
+    };
+    
+    const orderPromise = orderSunglasses();
+    
+    console.log(orderPromise);  
 
 //Node setTimeout() Function
 
-console.log("This is the first line of code in app.js.");
+    console.log("This is the first line of code in app.js.");
 
-const usingSTO = () => {
-  console.log("This line of code will log to the console last.");
-};
+    const usingSTO = () => {
+    console.log("This line of code will log to the console last.");
+    };
 
-setTimeout(usingSTO, 100);
+    setTimeout(usingSTO, 100);
 
-console.log("This is the last line of code in app.js.");
+    console.log("This is the last line of code in app.js.");
+
+//Success and Failure Callback Functions
+
+    const {checkInventory} = require('./library.js');
+
+    const order = [['sunglasses', 1], ['bags', 2]];
+
+    const handleSuccess = (resolvedValue) => {
+    console.log(resolvedValue);
+    };
+
+    const handleFailure = (rejectReason) => {
+    console.log(rejectReason);
+    };
+
+    checkInventory(order)
+    .then(handleSuccess, handleFailure);
