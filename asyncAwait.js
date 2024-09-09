295868c614f2d43aefb6335404340a2446334294
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-09 12:22:12 +0800

    async await - handling dependent promise

--- asyncAwait.js
+++ asyncAwait.js
@@ -59,3 +59,16 @@
     }
 
     getBeans();
+
+//Handling Dependent Promises
+
+    const {shopForBeans, soakTheBeans, cookTheBeans} = require('./library.js');
+
+    async function makeBeans() {
+    let type = await shopForBeans();
+    let isSoft = await soakTheBeans(type);
+    let dinner = await cookTheBeans(isSoft);
+    console.log(dinner);
+    }
+
+    makeBeans();
