10c2d3b67b45946b9a5020c5e370e116b6860b8f
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-09 12:05:50 +0800

    async await - the await operator

---
+++ asyncAwait.js
@@ -0,0 +1,49 @@
+//the async Keyword
+
+    function withConstructor(num){
+        return new Promise((resolve, reject) => {
+            if (num === 0){
+                resolve('zero');
+            } else {
+                resolve('not zero');
+            }
+        });
+    }
+    
+    withConstructor(0)
+        .then((resolveValue) => {
+        console.log(` withConstructor(0) returned a promise which resolved to: ${resolveValue}.`);
+    });
+    
+    async function withAsync(num){
+        if (num === 0){
+            return 'zero';
+        } else {
+            return 'not zero';
+        }
+    }
+    
+    withAsync(100)
+        .then((resolveValue) => {
+        console.log(` withAsync(100) returned a promise which resolved to: ${resolveValue}.`);
+    });
+
+//The Await Operator
+
+    const brainstormDinner = require('./library.js');
+
+    // Native promise version:
+    function nativePromiseDinner() {
+        brainstormDinner().then((meal) => {
+            console.log(`I'm going to make ${meal} for dinner.`);
+    });
+    }
+
+    // async/await version:
+    async function announceDinner() {
+        // Write your code below:
+        let meal = await brainstormDinner();
+        console.log(`I'm going to make ${meal} for dinner.`);
+    }
+
+    announceDinner();
